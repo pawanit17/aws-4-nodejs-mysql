0584717aa6d9bad8791d66194c4e946f97cd8e68
Pavan Dittakavi <pawan.it17@gmail.com>
2021-02-21 17:21:37 +0530

    Updated code

--- app.js
+++ app.js
@@ -1,17 +1,28 @@
-var mysql = require('mysql');
+// var mysql = require('mysql');
 
-var con = mysql.createConnection({
-  host: "cricinfo.cex37jklfrwo.ap-south-1.rds.amazonaws.com",
-  user: "cricinfo",
-  password: "cricinfo",
-  database: "cricketers"
+// var con = mysql.createConnection({
+//   host: "cricinfo.cex37jklfrwo.ap-south-1.rds.amazonaws.com",
+//   user: "cricinfo",
+//   password: "cricinfo",
+//   database: "cricketers"
+// });
+
+// con.connect(function(err) {
+//   if (err) throw err;
+//   console.log("Connected!");
+//   con.query("select * from cricketers_info", function (err, result, fields) {
+//     if (err) throw err;
+//     console.log(result);
+//   });
+// });
+
+const express = require('express');
+const app = express();
+
+app.get('/', (req,res) => {
+    res.send("I am here!.");
 });
 
-con.connect(function(err) {
-  if (err) throw err;
-  console.log("Connected!");
-  con.query("select * from cricketers_info", function (err, result, fields) {
-    if (err) throw err;
-    console.log(result);
-  });
+app.listen( process.env.port | 3000, () => {
+    console.log("Connected!");
 });
